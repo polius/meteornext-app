6e1203cd2ee6821bdd2fa92d1df654dc5f949884
Pol Alzina <gogs@fake.local>
2020-01-08 12:04:13 +0000

    Fixed a bug in Regions

--- server/routes/deployments/settings/regions.py
+++ server/routes/deployments/settings/regions.py
@@ -42,13 +42,13 @@ class Regions:
             region_json = request.get_json()
 
             if request.method == 'GET':
-                return self.__get(user['group_id'])
+                return self.get(user['group_id'])
             elif request.method == 'POST':
-                return self.__post(user['group_id'], region_json)
+                return self.post(user['group_id'], region_json)
             elif request.method == 'PUT':
-                return self.__put(user['group_id'], region_json)
+                return self.put(user['group_id'], region_json)
             elif request.method == 'DELETE':
-                return self.__delete(user['group_id'], region_json)
+                return self.delete(user['group_id'], region_json)
 
         @regions_blueprint.route('/deployments/regions/list', methods=['POST'])
         @jwt_required
@@ -104,10 +104,10 @@ class Regions:
     ####################
     # Internal Methods #
     ####################
-    def __get(self, group_id):
+    def get(self, group_id):
         return jsonify({'data': {'regions': self._regions.get(group_id), 'environments': self._environments.get(group_id)}}), 200
 
-    def __post(self, group_id, data):
+    def post(self, group_id, data):
         if self._regions.exist(group_id, data):
             return jsonify({'message': 'This region currently exists'}), 400
         elif data['cross_region']:
@@ -121,7 +121,7 @@ class Regions:
         self._regions.post(group_id, data)
         return jsonify({'message': 'Region added successfully'}), 200
 
-    def __put(self, group_id, data):
+    def put(self, group_id, data):
         if self._regions.exist(group_id, data):
             return jsonify({'message': 'This new region name currently exists'}), 400
         elif data['cross_region']:
@@ -146,7 +146,7 @@ class Regions:
         self._regions.put(group_id, data)
         return jsonify({'message': 'Region edited successfully'}), 200
 
-    def __delete(self, group_id, data):
+    def delete(self, group_id, data):
         # Check inconsistencies
         for region in data:
             if self._servers.exist_by_region(group_id, region):
