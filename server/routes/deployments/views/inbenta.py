3b1ef9b7077da98c89777194e02f1ffada909bcd
Pol Alzina <gogs@fake.local>
2020-01-13 10:58:20 +0000

    Bug fixes

--- server/routes/deployments/views/inbenta.py
+++ server/routes/deployments/views/inbenta.py
@@ -1,6 +1,7 @@
 import os
 import json
 import signal
+from datetime import datetime
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import (jwt_required, get_jwt_identity)
 
--- server/routes/deployments/views/pro.py
+++ server/routes/deployments/views/pro.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import signal
+from datetime import datetime
 from flask import Blueprint, jsonify, request
 from flask_jwt_extended import (jwt_required, get_jwt_identity)
 
