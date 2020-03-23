996479babfa2e32c760afaea1b4047b6415b20a2
Pol Alzina <gogs@fake.local>
2020-03-23 17:21:13 +0000

    More

--- client/src/components/client/Navigation.vue
+++ client/src/components/client/Navigation.vue
@@ -3,7 +3,7 @@
     <div>
       <v-tabs background-color="#9b59b6" color="white" v-model="tabs" slider-color="white" slot="extension" class="elevation-2">
         <v-tabs-slider></v-tabs-slider>
-          <v-tab to="/jobs"><span class="pl-2 pr-2"><v-icon small style="padding-right:10px">fas fa-bolt</v-icon>JOBS</span></v-tab>
+          <v-tab to="/client"><span class="pl-2 pr-2"><v-icon small style="padding-right:10px">fas fa-bolt</v-icon>CLIENT</span></v-tab>
           <v-divider class="mx-3" inset vertical></v-divider>
         </v-tabs>
     </div>
