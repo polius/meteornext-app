037e7bf31edb424c67c16c7a3249f988e73943f1
Pol Alzina <gogs@fake.local>
2020-03-26 16:47:03 +0000

    Improvements

--- client/src/components/utils/Navigation.vue
+++ client/src/components/utils/Navigation.vue
@@ -5,7 +5,7 @@
         <v-tabs-slider></v-tabs-slider>
           <v-tab to="/utils"><span class="pl-2 pr-2"><v-icon small style="padding-right:10px">fas fa-database</v-icon>UTILS</span></v-tab>
           <v-divider class="mx-3" inset vertical></v-divider>
-          <v-tab to="/utils/users"><span class="pl-2 pr-2">User Management</span></v-tab>
+          <v-tab to="/utils/manage"><span class="pl-2 pr-2">User Management</span></v-tab>
           <v-tab to="/utils/compare"><span class="pl-2 pr-2">SQL Compare</span></v-tab>
           <v-tab to="/utils/restore"><span class="pl-2 pr-2">SQL Restore</span></v-tab>
         </v-tabs>
