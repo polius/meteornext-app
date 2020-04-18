fd6691d68f0fea2026f54ecaff28bfdc4a6b3503
Pol Alzina <gogs@fake.local>
2020-04-18 17:06:48 +0000

    More code

--- server/apps/monitoring/monitoring.py
+++ server/apps/monitoring/monitoring.py
@@ -1,6 +1,8 @@
 import json
 import datetime
 import threading
+from collections import OrderedDict
+
 from connector import connector
 
 class Monitoring:
@@ -54,11 +56,11 @@ class Monitoring:
             if server['summary'] is None:
                 summary = {'info': {'available': False}}
                 query = "UPDATE monitoring SET summary = %s WHERE server_id = %s"
-                self._sql.execute(query=query, args=(self.__dict2str(summary), server['id']))
             else:
-                server['summary']['info']['available'] = False
+                summary = self.__str2dict(server['summary'])
+                summary['info']['available'] = False
                 query = "UPDATE monitoring SET summary = %s WHERE server_id = %s"
-                self._sql.execute(query=query, args=(self.__dict2str(server['summary']), server['id']))
+            self._sql.execute(query=query, args=(self.__dict2str(summary), server['id']))
         else:
             # Get Server Info
             params = conn.get_parameters()
@@ -71,7 +73,7 @@ class Monitoring:
             summary = {'info': {}, 'logs': {}, 'connections': {}, 'statements': {}, 'index': {}, 'rds': {}}
             summary['info'] = {'available': True, 'version': params.get('version'), 'uptime': str(datetime.timedelta(seconds=int(status['Uptime']))), 'start_time': str((datetime.datetime.now() - datetime.timedelta(seconds=int(status['Uptime']))).replace(microsecond=0)), 'engine': params.get('default_storage_engine'), 'allocated_memory': params.get('innodb_buffer_pool_size'), 'time_zone': params.get('time_zone')}
             summary['logs'] = {'general_log': params.get('general_log'), 'general_log_file': params.get('general_log_file'), 'slow_log': params.get('slow_query_log'), 'slow_log_file': params.get('slow_query_log_file'), 'error_log_file': params.get('log_error')}
-            summary['connections'] = {'max_connections_allowed': params.get('max_connections'), 'max_connections_reached': "{:.2f}%".format((int(status.get('Max_used_connections')) / int(params.get('max_connections'))) * 100), 'max_allowed_packet': params.get('max_allowed_packet'), 'transaction_isolation': params.get('tx_isolation'), 'bytes_received': status.get('Bytes_received'), 'bytes_sent': status.get('Bytes_sent')}
+            summary['connections'] = {'current': status.get('Threads_connected'), 'max_connections_allowed': params.get('max_connections'), 'max_connections_reached': "{:.2f}%".format((int(status.get('Max_used_connections')) / int(params.get('max_connections'))) * 100), 'max_allowed_packet': params.get('max_allowed_packet'), 'transaction_isolation': params.get('tx_isolation'), 'bytes_received': status.get('Bytes_received'), 'bytes_sent': status.get('Bytes_sent')}
             summary['statements'] = {'all': status.get('Questions'), 'select': int(status.get('Com_select')) + int(status.get('Qcache_hits')), 'insert': int(status.get('Com_insert')) + int(status.get('Com_insert_select')), 'update': int(status.get('Com_update')) + int(status.get('Com_update_multi')), 'delete': int(status.get('Com_delete')) + int(status.get('Com_delete_multi'))}
             summary['index'] = {'percent': "{:.2f}%".format((int(status['Handler_read_rnd_next']) + int(status['Handler_read_rnd'])) / (int(status['Handler_read_rnd_next']) + int(status['Handler_read_rnd']) + int(status['Handler_read_first']) + int(status['Handler_read_next']) + int(status['Handler_read_key']) + int(status['Handler_read_prev']))), 'selects': status['Select_scan']}
             summary['rds'] = {}
@@ -92,3 +94,7 @@ class Monitoring:
 
     def __dict2str(self, data):
         return json.dumps(data, separators=(',', ':'))
+
+    def __str2dict(self, data):
+        # Convert a string representation of a dictionary to a dictionary
+        return json.loads(data, object_pairs_hook=OrderedDict)
