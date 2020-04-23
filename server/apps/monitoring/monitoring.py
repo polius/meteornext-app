a011d099df3386afd32398b961da306696d7a642
Pol Alzina <gogs@fake.local>
2020-04-23 17:05:30 +0000

    Improved Monitoring Backend

--- server/apps/monitoring/monitoring.py
+++ server/apps/monitoring/monitoring.py
@@ -35,7 +35,7 @@ class Monitoring:
             server['id'] = s['id']
             server['ssh'] = {'enabled': s['ssh_tunnel'], 'hostname': s['hostname'], 'port': s['rport'], 'username': s['rusername'], 'password': s['rpassword'], 'key': s['key']}
             server['sql'] = {'engine': s['engine'], 'hostname': s['hostname'], 'port': s['port'], 'username': s['username'], 'password': s['password']}
-            server['summary'] = s['summary']
+            server['monitor'] = {'summary': s['summary'], 'monitor_enabled': s['monitor_enabled'], 'parameters_enabled': s['parameters_enabled'], 'processlist_enabled': s['processlist_enabled'], 'queries_enabled': s['queries_enabled']}
             servers.append(server)
 
         # Get Server Info
@@ -70,41 +70,52 @@ class Monitoring:
             conn.start()
         except Exception:
             # Set Server Available to False
-            if server['summary'] is None:
+            if server['monitor']['summary'] is None:
                 summary = {'info': {'available': False}}
-                query = "UPDATE monitoring SET summary = %s, updated = CURRENT_TIMESTAMP WHERE server_id = %s"
+                query = "UPDATE monitoring SET summary = %s AND updated = CURRENT_TIMESTAMP WHERE server_id = %s"
             else:
-                summary = self.__str2dict(server['summary'])
+                summary = self.__str2dict(server['monitor']['summary'])
                 summary['info']['available'] = False
                 query = "UPDATE monitoring SET summary = %s, updated = CURRENT_TIMESTAMP WHERE server_id = %s"
             self._sql.execute(query=query, args=(self.__dict2str(summary), server['id']))
         else:
-            # Get Server Info
-            params = conn.get_parameters()
-            status = conn.get_status()
-            processlist = conn.get_processlist()
-            params = { p['Variable_name']: p['Value'] for p in params }
-            status = { s['Variable_name']: s['Value'] for s in status }
+            # Build Parameters
+            params = ''
+            if server['monitor']['monitor_enabled'] or server['monitor']['parameters_enabled']:
+                params = { p['Variable_name']: p['Value'] for p in conn.get_parameters() }
+                status = { s['Variable_name']: s['Value'] for s in conn.get_status() }
 
+            # Build Processlist
+            processlist = ''
+            if server['monitor']['monitor_enabled'] or server['monitor']['processlist_enabled']:
+                processlist = conn.get_processlist()
+            
             # Build Summary
-            summary = {'info': {}, 'logs': {}, 'connections': {}, 'statements': {}, 'index': {}, 'rds': {}}
-            summary['info'] = {'available': True, 'version': params.get('version'), 'uptime': str(datetime.timedelta(seconds=int(status['Uptime']))), 'start_time': str((datetime.datetime.now() - datetime.timedelta(seconds=int(status['Uptime']))).replace(microsecond=0)), 'engine': server['sql']['engine'], 'sql_engine': params.get('default_storage_engine'), 'allocated_memory': params.get('innodb_buffer_pool_size'), 'time_zone': params.get('time_zone')}
-            summary['logs'] = {'general_log': params.get('general_log'), 'general_log_file': params.get('general_log_file'), 'slow_log': params.get('slow_query_log'), 'slow_log_file': params.get('slow_query_log_file'), 'error_log_file': params.get('log_error')}
-            summary['connections'] = {'current': status.get('Threads_connected'), 'max_connections_allowed': params.get('max_connections'), 'max_connections_reached': "{:.2f}%".format((int(status.get('Max_used_connections')) / int(params.get('max_connections'))) * 100), 'max_allowed_packet': params.get('max_allowed_packet'), 'transaction_isolation': params.get('tx_isolation'), 'bytes_received': status.get('Bytes_received'), 'bytes_sent': status.get('Bytes_sent')}
-            summary['statements'] = {'all': status.get('Questions'), 'select': int(status.get('Com_select')) + int(status.get('Qcache_hits')), 'insert': int(status.get('Com_insert')) + int(status.get('Com_insert_select')), 'update': int(status.get('Com_update')) + int(status.get('Com_update_multi')), 'delete': int(status.get('Com_delete')) + int(status.get('Com_delete_multi'))}
-            summary['index'] = {'percent': "{:.2f}%".format((int(status['Handler_read_rnd_next']) + int(status['Handler_read_rnd'])) / (int(status['Handler_read_rnd_next']) + int(status['Handler_read_rnd']) + int(status['Handler_read_first']) + int(status['Handler_read_next']) + int(status['Handler_read_key']) + int(status['Handler_read_prev']))), 'selects': status['Select_scan']}
-            summary['rds'] = {}
+            summary = ''
+            if server['monitor']['monitor_enabled']:
+                summary = {'info': {}, 'logs': {}, 'connections': {}, 'statements': {}, 'index': {}, 'rds': {}}
+                summary['info'] = {'available': True, 'version': params.get('version'), 'uptime': str(datetime.timedelta(seconds=int(status['Uptime']))), 'start_time': str((datetime.datetime.now() - datetime.timedelta(seconds=int(status['Uptime']))).replace(microsecond=0)), 'engine': server['sql']['engine'], 'sql_engine': params.get('default_storage_engine'), 'allocated_memory': params.get('innodb_buffer_pool_size'), 'time_zone': params.get('time_zone')}
+                summary['logs'] = {'general_log': params.get('general_log'), 'general_log_file': params.get('general_log_file'), 'slow_log': params.get('slow_query_log'), 'slow_log_file': params.get('slow_query_log_file'), 'error_log_file': params.get('log_error')}
+                summary['connections'] = {'current': status.get('Threads_connected'), 'max_connections_allowed': params.get('max_connections'), 'max_connections_reached': "{:.2f}%".format((int(status.get('Max_used_connections')) / int(params.get('max_connections'))) * 100), 'max_allowed_packet': params.get('max_allowed_packet'), 'transaction_isolation': params.get('tx_isolation'), 'bytes_received': status.get('Bytes_received'), 'bytes_sent': status.get('Bytes_sent')}
+                summary['statements'] = {'all': status.get('Questions'), 'select': int(status.get('Com_select')) + int(status.get('Qcache_hits')), 'insert': int(status.get('Com_insert')) + int(status.get('Com_insert_select')), 'update': int(status.get('Com_update')) + int(status.get('Com_update_multi')), 'delete': int(status.get('Com_delete')) + int(status.get('Com_delete_multi'))}
+                summary['index'] = {'percent': "{:.2f}%".format((int(status['Handler_read_rnd_next']) + int(status['Handler_read_rnd'])) / (int(status['Handler_read_rnd_next']) + int(status['Handler_read_rnd']) + int(status['Handler_read_first']) + int(status['Handler_read_next']) + int(status['Handler_read_key']) + int(status['Handler_read_prev']))), 'selects': status['Select_scan']}
+                summary['rds'] = {}
+
+            # Parse Variables
+            summary = self.__dict2str(summary) if summary != '' else ''
+            params = self.__dict2str(params) if params != '' else ''
+            processlist = self.__dict2str(processlist) if processlist != '' else ''
 
             # Store Variables
             query = """
                 UPDATE monitoring
-                SET summary = %s,
-                    parameters = %s,
-                    processlist = %s,
+                SET summary = IF(%s = '', NULL, %s),
+                    parameters = IF(%s = '', NULL, %s),
+                    processlist = IF(%s = '', NULL, %s),
                     updated = CURRENT_TIMESTAMP
                 WHERE server_id = %s
             """
-            self._sql.execute(query=query, args=(self.__dict2str(summary), self.__dict2str(params), self.__dict2str(processlist), server['id']))
+            self._sql.execute(query=query, args=(summary, summary, params, params, processlist, processlist, server['id']))
 
         finally:
             # Stop Connection
