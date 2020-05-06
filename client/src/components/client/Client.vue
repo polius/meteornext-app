4989185d5462401c84b3c8e52ea3f945dc7a01e3
Pol Alzina <gogs@fake.local>
2020-05-06 17:38:36 +0000

    More code

--- client/src/components/client/Client.vue
+++ client/src/components/client/Client.vue
@@ -7,7 +7,8 @@
       <Pane size="80" min-size="10">
         <Splitpanes horizontal>
           <Pane size="90">
-            <span>2</span>
+            <!-- <span>2</span> -->
+            <div id="editor" style="height:100vh"></div>
           </Pane>
           <Pane size="10" min-size="10">
             <span>3</span>
@@ -35,18 +36,31 @@
 .splitpanes--horizontal > .splitpanes__splitter {
   min-height: 3px;
 }
-
+.ace_editor {
+  margin: auto;
+  height: 100%;
+  width: 100%;
+}
+#editor {
+  height: 100%;
+}
 </style>
 
 <script>
 import { Splitpanes, Pane } from 'splitpanes'
 import 'splitpanes/dist/splitpanes.css'
 
+import * as ace from 'ace-builds';
+import 'ace-builds/webpack-resolver';
+import 'ace-builds/src-noconflict/ext-language_tools';
+
 export default {
   data() {
     return {
-      
-      
+      // ACE Editor
+      editor: '',
+      editorTools: null,
+
       // Snackbar
       snackbar: false,
       snackbarTimeout: Number(3000),
@@ -55,9 +69,45 @@ export default {
     }
   },
   components: { Splitpanes, Pane },
-  created() {
+  mounted() {
+    // Create Editor
+    this.editor = ace.edit("editor", {
+      mode: "ace/mode/sql",
+      theme: "ace/theme/monokai",
+      fontSize: 14,
+      showPrintMargin: false,
+      wrap: true,
+      autoScrollEditorIntoView: true,
+      enableBasicAutocompletion: true,
+      enableLiveAutocompletion: true,
+      enableSnippets: false
+    });
+
+    var myList = [
+      "/dev/sda1",
+      "/dev/sda2"
+    ]
+
+    this.editorTools = ace.require("ace/ext/language_tools");
+    var myCompleter = {
+      identifierRegexps: [/[^\s]+/],
+      getCompletions: function(editor, session, pos, prefix, callback) {
+        callback(
+          null,
+          myList.filter(entry=>{
+            return entry.includes(prefix);
+          }).map(entry=>{
+            return {
+              value: entry
+            };
+          })
+        );
+      }
+    }
+    this.editorTools.addCompleter(myCompleter);
   },
   methods: {
+    
   }
 }
 </script>
