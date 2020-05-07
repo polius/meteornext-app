2b5d9ca14c5177fd99eb191aa0f81c199432365c
Pol Alzina <gogs@fake.local>
2020-05-07 10:52:12 +0000

    More code

--- client/src/components/client/Client.vue
+++ client/src/components/client/Client.vue
@@ -2,16 +2,40 @@
   <div style="height: calc(100vh - 112px)">
     <Splitpanes>
       <Pane size="20" min-size="10">
-        <span>1</span>
+        <div style="margin-left:auto; margin-right:auto; height:100%; width:100%">
+          <v-treeview v-model="tree" :open="open" :items="items" activatable item-key="name" style="height:calc(100% - 58px); width:100%; overflow-y:auto;">
+            <template v-slot:label="{item, open}">        
+              <v-btn text @contextmenu="show" style="font-size:14px; text-transform:none; font-weight:400; width:100%; justify-content:left; padding:0px;"> 
+                <!--button icon-->
+                <v-icon v-if="!item.file" small style="padding:10px;">
+                  {{ open ? 'mdi-folder-open' : 'mdi-folder' }}
+                </v-icon>
+                <v-icon v-else small style="padding:10px;">
+                  {{ files[item.file] }}
+                </v-icon>
+                <!--button text-->
+                {{item.name}}                  
+              </v-btn>
+            </template>
+          </v-treeview>
+          <v-menu v-model="showMenu" :position-x="x" :position-y="y" absolute offset-y>
+            <v-list>
+              <v-list-item v-for="menuItem in menuItems" :key="menuItem" @click="clickAction">
+                <v-list-item-title>{{menuItem}}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+          <v-text-field v-model="search" label="Search" dense solo hide-details style="float:left; width:100%; padding:10px;"></v-text-field>
+        </div>
       </Pane>
       <Pane size="80" min-size="10">
         <Splitpanes horizontal>
-          <Pane size="90">
-            <!-- <span>2</span> -->
-            <div id="editor" style="height:100vh"></div>
+          <Pane size="100">
+            <div id="editor"></div>
           </Pane>
-          <Pane size="10" min-size="10">
-            <span>3</span>
+          <Pane size="0" min-size="10">
+            <v-data-table :headers="resultsHeaders" :items="resultsItems" :hide-default-footer="resultsItems.length < 11" class="elevation-1" style="height:100%; width:100%; border-radius:0px;">
+            </v-data-table>
           </Pane>
         </Splitpanes>
       </Pane>
@@ -41,8 +65,8 @@
   height: 100%;
   width: 100%;
 }
-#editor {
-  height: 100%;
+.v-treeview-node__root {
+  min-height:40px;
 }
 </style>
 
@@ -57,9 +81,86 @@ import 'ace-builds/src-noconflict/ext-language_tools';
 export default {
   data() {
     return {
+      // Servers Tree View
+      open: ["public"],
+      files: {
+        html: "mdi-language-html5",
+        js: "mdi-nodejs",
+        json: "mdi-json",
+        md: "mdi-markdown",
+        pdf: "mdi-file-pdf",
+        png: "mdi-file-image",
+        txt: "mdi-file-document-outline",
+        xls: "mdi-file-excel"
+      },
+      tree: [],
+      items: [
+        {
+          name: ".git"
+        },
+        {
+          name: "node_modules"
+        },
+        {
+          name: "public",
+          children: [
+            {
+              name: "static",
+              children: [
+                {
+                  name: "logo.png",
+                  file: "png"
+                }
+              ]
+            },
+            {
+              name: "favicon.ico",
+              file: "png"
+            },
+            {
+              name: "index.html",
+              file: "html"
+            }
+          ]
+        },
+        {
+          name: ".gitignore",
+          file: "txt"
+        },
+        {
+          name: "babel.config.js",
+          file: "js"
+        },
+        {
+          name: "package.json",
+          file: "json"
+        },
+        {
+          name: "README.md",
+          file: "md"
+        },
+        {
+          name: "vue.config.js",
+          file: "js"
+        },
+        {
+          name: "yarn.lock",
+          file: "txt"
+        }
+      ],
+      showMenu: false,
+      x: 0,
+      y: 0,
+      menuItems: ["create file", "create directory"],
+      search: '',
+
       // ACE Editor
       editor: '',
       editorTools: null,
+
+      // Results Table Data
+      resultsHeaders: [],
+      resultsItems: [],
 
       // Snackbar
       snackbar: false,
@@ -82,6 +183,7 @@ export default {
       enableLiveAutocompletion: true,
       enableSnippets: false
     });
+    this.editor.session.setOptions({ tabSize: 4, useSoftTabs: false });
 
     var myList = [
       "/dev/sda1",
@@ -107,7 +209,18 @@ export default {
     this.editorTools.addCompleter(myCompleter);
   },
   methods: {
-    
+    clickAction(){
+      alert('clicked');
+    },
+    show(e) {
+      e.preventDefault();
+      this.showMenu = false;
+      this.x = e.clientX;
+      this.y = e.clientY;
+      this.$nextTick(() => {
+        this.showMenu = true;
+      });
+    }
   }
 }
 </script>
