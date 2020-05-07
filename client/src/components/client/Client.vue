fe32e49e93d6973a7bde3e9c2f8a83b2d5c286de
Pol Alzina <gogs@fake.local>
2020-05-07 15:41:43 +0000

    More

--- client/src/components/client/Client.vue
+++ client/src/components/client/Client.vue
@@ -37,6 +37,8 @@
                 <v-tab><span class="pl-2 pr-2"><v-btn small icon style="margin-right:10px;"><v-icon x-small style="padding-bottom:1px;">fas fa-times</v-icon></v-btn>Connection 1</span></v-tab>
                 <v-divider class="mx-3" inset vertical></v-divider>
                 <v-tab><span class="pl-2 pr-2" style="font-size:18px;">+</span></v-tab>
+                <v-spacer></v-spacer>
+                <v-btn style="margin:6px;" title="Execute Query"><v-icon small style="padding-right:10px;">fas fa-bolt</v-icon>Run</v-btn>
               </v-tabs>
               <div id="editor" style="float:left"></div>
             </div>
