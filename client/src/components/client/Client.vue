ace0d6865fe060dd6ce0710623a99d7bf4db9271
Pol Alzina <gogs@fake.local>
2020-05-07 15:31:51 +0000

    More

--- client/src/components/client/Client.vue
+++ client/src/components/client/Client.vue
@@ -31,7 +31,15 @@
       <Pane size="80" min-size="10">
         <Splitpanes horizontal @ready="initAce()" @resize="resize()">
           <Pane size="90">
-            <div id="editor"></div>
+            <div style="margin-left:auto; margin-right:auto; height:100%; width:100%">
+              <v-tabs dense show-arrows background-color="#303030" color="white" v-model="tabs" slider-color="white" slot="extension" class="elevation-2" style="width:100%;">
+                <v-tabs-slider></v-tabs-slider>
+                <v-tab><span class="pl-2 pr-2"><v-btn small icon style="margin-right:10px;"><v-icon x-small style="padding-bottom:1px;">fas fa-times</v-icon></v-btn>Connection 1</span></v-tab>
+                <v-divider class="mx-3" inset vertical></v-divider>
+                <v-tab><span class="pl-2 pr-2" style="font-size:18px;">+</span></v-tab>
+              </v-tabs>
+              <div id="editor" style="float:left"></div>
+            </div>
           </Pane>
           <Pane size="10" min-size="10">
             <v-data-table :headers="resultsHeaders" :items="resultsItems" :hide-default-footer="resultsItems.length < 11" class="elevation-1" style="height:100%; width:100%; border-radius:0px; background-color:#303030;">
@@ -101,6 +109,9 @@ import 'ace-builds/src-noconflict/ext-language_tools';
 export default {
   data() {
     return {
+      // Tabs
+      tabs: [],
+
       // Servers Tree View
       open: ["public"],
       files: {
