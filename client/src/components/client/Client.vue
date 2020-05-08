f40d100f6cb71b0ab0e2a29eb0980d6992e7cc92
Pol Alzina <gogs@fake.local>
2020-05-08 16:56:06 +0000

    More

--- client/src/components/client/Client.vue
+++ client/src/components/client/Client.vue
@@ -34,7 +34,7 @@
         <Splitpanes horizontal @ready="initAce()" @resize="resize($event)">
           <Pane size="100">
             <div style="margin-left:auto; margin-right:auto; height:100%; width:100%">
-              <v-tabs dense background-color="#303030" color="white" v-model="tabs" slider-color="white" slot="extension" class="elevation-2" style="max-width:calc(100% - 97px); float:left;">
+              <v-tabs show-arrows dense background-color="#303030" color="white" v-model="tabs" slider-color="white" slot="extension" class="elevation-2" style="max-width:calc(100% - 97px); float:left;">
                 <v-tabs-slider></v-tabs-slider>
                 <v-tab><span class="pl-2 pr-2"><v-btn small icon style="margin-right:10px;"><v-icon x-small style="padding-bottom:1px;">fas fa-times</v-icon></v-btn>Connection 1</span></v-tab>
                 <v-divider class="mx-3" inset vertical></v-divider>
