d824596cd8eb1337a8d1dd7a78ae88845f128515
Pol Alzina <gogs@fake.local>
2020-05-13 14:22:46 +0000

    Improvements

--- client/src/components/client/Navigation.vue
+++ client/src/components/client/Navigation.vue
@@ -19,7 +19,8 @@
           <v-divider class="mx-3" inset vertical></v-divider>
           <v-spacer></v-spacer>
           <v-tab title="Users" style="min-width:10px;"><span class="pl-2 pr-2"><v-icon small>fas fa-user-lock</v-icon></span></v-tab>
-          <v-tab title="History" style="min-width:10px;"><span class="pl-2 pr-2"><v-icon small>fas fa-history</v-icon></span></v-tab>
+          <v-tab title="Saved Queries" style="min-width:10px;"><span class="pl-2 pr-2"><v-icon small>fas fa-star</v-icon></span></v-tab>
+          <v-tab title="Query History" style="min-width:10px;"><span class="pl-2 pr-2"><v-icon small>fas fa-history</v-icon></span></v-tab>
         </v-tabs>
     </div>
     <v-container fluid>
