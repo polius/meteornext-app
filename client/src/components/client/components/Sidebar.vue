02a531e57f2f744f40157b1d5eb870402ff76e9f
Pol Alzina <gogs@fake.local>
2020-08-24 11:58:26 +0000

    More code

--- client/src/components/client/components/Sidebar.vue
+++ client/src/components/client/components/Sidebar.vue
@@ -221,6 +221,7 @@ export default {
       // Get Column Types + Collations
       if (server.type == 'MySQL') {
         this.server.columnTypes = ['TINYINT','SMALLINT','MEDIUMINT','INT','BIGINT','FLOAT','DOUBLE','BIT','CHAR','VARCHAR','BINARY','VARBINARY','TINYBLOB','BLOB','MEDIUMBLOB','LONGBLOB','TINYTEXT','TEXT','MEDIUMTEXT','LONGTEXT','ENUM','SET','DATE','TIME','DATETIME','TIMESTAMP','YEAR','GEOMETRY','POINT','LINESTRING','POLYGON','GEOMETRYCOLLECTION','MULTILINESTRING','MULTIPOINT','MULTIPOLYGON','JSON']
+        this.server.indexTypes = ['INDEX','UNIQUE','FULLTEXT']
         this.server.collations = data.collations
       }
     },
