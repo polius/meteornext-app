524199002c06b7aed109e4492bea7be0ce2fd381
Pol Alzina <gogs@fake.local>
2020-09-06 16:48:14 +0000

    More code

--- client/src/components/client/components/Header.vue
+++ client/src/components/client/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-tabs show-arrows background-color="#9b59b6" color="white" v-model="headerTab" slider-color="white" slot="extension" class="elevation-2">
+    <v-tabs v-model="headerTab" show-arrows background-color="#9b59b6" color="white" slider-color="white" slot="extension" class="elevation-2">
       <v-tabs-slider></v-tabs-slider>
       <v-tab @click="tabClient()"><span class="pl-2 pr-2"><v-icon small style="padding-right:10px">fas fa-bolt</v-icon>CLIENT</span></v-tab>
       <v-divider class="mx-3" inset vertical></v-divider>
