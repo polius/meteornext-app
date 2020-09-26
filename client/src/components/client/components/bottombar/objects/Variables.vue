31bbdd3aacbc4f507b3ae27ccb916d7d8630aa58
Pol Alzina <gogs@fake.local>
2020-09-26 08:48:23 +0000

    More code

--- client/src/components/client/components/bottombar/objects/Variables.vue
+++ client/src/components/client/components/bottombar/objects/Variables.vue
@@ -12,7 +12,37 @@
             <v-layout wrap>
               <v-flex xs12>
                 <v-text-field ref="field" :disabled="loading" v-model="search" label="Filter..." solo dense clearable hide-details></v-text-field>
-                <ag-grid-vue suppressColumnVirtualisation suppressRowClickSelection @grid-ready="onGridReady" @first-data-rendered="onFirstDataRendered" style="width:100%; height:70vh;" class="ag-theme-alpine-dark" rowHeight="35" headerHeight="35" rowSelection="single" :columnDefs="columns" :rowData="items"></ag-grid-vue>
+                <ag-grid-vue suppressColumnVirtualisation suppressRowClickSelection @grid-ready="onGridReady" @first-data-rendered="onFirstDataRendered" @cell-editing-started="cellEditingStarted" @cell-editing-stopped="cellEditingStopped" style="width:100%; height:70vh;" class="ag-theme-alpine-dark" rowHeight="35" headerHeight="35" rowSelection="single" :columnDefs="columns" :rowData="items"></ag-grid-vue>
+              </v-flex>
+            </v-layout>
+          </v-container>
+        </v-card-text>
+      </v-card>
+    </v-dialog>
+    <!-------------------->
+    <!-- CONFIRM DIALOG -->
+    <!-------------------->
+    <v-dialog v-model="confirmDialog" max-width="50%">
+      <v-card>
+        <v-card-text style="padding:15px 15px 5px;">
+          <v-container style="padding:0px; max-width:100%;">
+            <v-layout wrap>
+              <div class="text-h6" style="font-weight:400;">Confirmation</div>
+              <v-flex xs12>
+                <div class="body-1" style="font-weight:300; font-size:1.05rem!important; margin-top:10px;">Are you sure you want to change this server variable?</div>
+                <v-text-field v-model="currentCellValues.current" filled readonly label="Current Value" hide-details style="margin-top:15px;"></v-text-field>
+                <v-text-field v-model="currentCellValues.new" filled readonly label="New Value" hide-details style="margin-top:15px; margin-bottom:15px;"></v-text-field>
+                <v-divider></v-divider>
+                <div style="margin-top:15px;">
+                  <v-row no-gutters>
+                    <v-col cols="auto" style="margin-right:5px; margin-bottom:10px;">
+                      <v-btn :loading="loading" @click="confirmDialogSubmit" color="primary">Confirm</v-btn>
+                    </v-col>
+                    <v-col style="margin-bottom:10px;">
+                      <v-btn :disabled="loading" @click="confirmDialogCancel" outlined color="#e74d3c">Cancel</v-btn>
+                    </v-col>
+                  </v-row>
+                </div>                
               </v-flex>
             </v-layout>
           </v-container>
@@ -44,9 +74,13 @@ export default {
       // Grid Api
       gridApi: null,
       columnApi: null,
+      currentCellEditNode: {},
+      currentCellValues: {'variable': '', 'current': '', 'new': ''},
       columns: [],
       items: [],
       search: '',
+      // Confirm Dialog
+      confirmDialog: false,
     }
   },
   components: { AgGridVue },
@@ -109,6 +143,36 @@ export default {
     },
     onFirstDataRendered(params) {
       params.api.sizeColumnsToFit()
+    },
+    cellEditingStarted(event) {
+      // Store row node
+      this.currentCellEditNode = event.node
+      // Store current value
+      this.currentCellValues = {'variable': event.data['variable'], 'current': event.data['value']}
+    },
+    cellEditingStopped(event) {
+      // Store new value
+      this.currentCellValues['new'] = event.value
+      // Open confirmation dialog
+      if (this.currentCellValues['current'] != this.currentCellValues['new']) this.confirmDialog = true
+    },
+    confirmDialogSubmit() {
+      this.loading = true
+      let query = "SET GLOBAL " + this.currentCellValues['variable'] + ' = ' + this.currentCellValues['new']
+      new Promise((resolve, reject) => { 
+        EventBus.$emit('EXECUTE_SIDEBAR', [query], resolve, reject)
+      }).then(() => { 
+        // Change Variable
+        this.currentCellEditNode.setDataValue('value', this.currentCellValues['new'])
+        // Hide Confirm Dialog
+        this.confirmDialog = false
+        // Send notification
+        EventBus.$emit('SEND_NOTIFICATION', 'Server variable changed successfully', 'success')
+      }).catch(() => {}).finally(() => { this.loading = false })
+    },
+    confirmDialogCancel() {
+      this.currentCellEditNode.setDataValue('value', this.currentCellValues['current'])
+      this.confirmDialog = false
     },
   }
 }
