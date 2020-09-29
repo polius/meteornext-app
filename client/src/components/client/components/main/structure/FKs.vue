b3a12c88cf96a3ddb012c1f55359d3ebd6a6eceb
Pol Alzina <gogs@fake.local>
2020-09-29 18:23:29 +0000

    More code

--- client/src/components/client/components/main/structure/FKs.vue
+++ client/src/components/client/components/main/structure/FKs.vue
@@ -4,7 +4,7 @@
     <!-- FKs -->
     <!--------->
     <div style="height:calc(100% - 84px)">
-      <ag-grid-vue ref="agGridStructureFKs" suppressContextMenu preventDefaultOnContextMenu @grid-ready="onGridReady" @new-columns-loaded="onNewColumnsLoaded" @cell-key-down="onCellKeyDown" @cell-clicked="onCellClicked" style="width:100%; height:100%;" class="ag-theme-alpine-dark" rowDragManaged="true" suppressMoveWhenRowDragging="true" rowHeight="35" headerHeight="35" rowSelection="single" rowDeselection="true" stopEditingWhenGridLosesFocus="true" :columnDefs="structureHeaders.fks" :rowData="structureItems.fks"></ag-grid-vue>
+      <ag-grid-vue ref="agGridStructureFKs" suppressDragLeaveHidesColumns suppressContextMenu preventDefaultOnContextMenu @grid-ready="onGridReady" @new-columns-loaded="onNewColumnsLoaded" @cell-key-down="onCellKeyDown" @cell-clicked="onCellClicked" style="width:100%; height:100%;" class="ag-theme-alpine-dark" rowDragManaged="true" suppressMoveWhenRowDragging="true" rowHeight="35" headerHeight="35" rowSelection="single" rowDeselection="true" stopEditingWhenGridLosesFocus="true" :columnDefs="structureHeaders.fks" :rowData="structureItems.fks"></ag-grid-vue>
     </div>
     <!---------------->
     <!-- BOTTOM BAR -->
