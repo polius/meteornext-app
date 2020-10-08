93bce6182ce4022f8b501a10685ee9d09ca10ba1
Pol Alzina <gogs@fake.local>
2020-10-08 13:53:00 +0000

    More code

---
+++ client/src/components/client/components/header/rights/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div style="height:100%">
+    
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+
+    }
+  },
+  computed: {
+  },
+  methods: {
+
+  }
+}
+</script>
---
+++ client/src/components/client/components/header/rights/Schema.vue
@@ -0,0 +1,30 @@
+<template>
+  <div style="height:100%">
+    <div style="height: calc(100% - 84px)">
+                            
+    </div>
+    <v-row no-gutters style="height:35px; border-top:2px solid #3b3b3b; width:100%">
+      <v-btn text small title="New User Right" style="height:30px; min-width:36px; margin-top:1px; margin-left:2px; margin-right:2px;"><v-icon small style="font-size:12px;">fas fa-plus</v-icon></v-btn>
+      <span style="background-color:#3b3b3b; padding-left:1px;margin-left:1px; margin-right:1px;"></span>
+      <v-btn text small title="Delete User Right" style="height:30px; min-width:36px; margin-top:1px; margin-left:2px; margin-right:2px;"><v-icon small style="font-size:12px;">fas fa-minus</v-icon></v-btn>
+      <span style="background-color:#3b3b3b; padding-left:1px;margin-left:1px; margin-right:1px;"></span>
+      <v-btn text small title="Refresh" style="height:30px; min-width:36px; margin-top:1px; margin-left:2px; margin-right:2px;"><v-icon small style="font-size:12px;">fas fa-redo-alt</v-icon></v-btn>
+      <span style="background-color:#3b3b3b; padding-left:1px;margin-left:1px; margin-right:1px;"></span>
+    </v-row>
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+    }
+  },
+  computed: {
+  },
+  methods: {
+
+  }
+}
+</script>
