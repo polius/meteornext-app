31286f34cffd75281ed8fde635fcd196b7fc3953
Pol Alzina <gogs@fake.local>
2020-10-09 10:49:37 +0000

    More code

--- client/src/components/client/components/header/rights/Login.vue
+++ client/src/components/client/components/header/rights/Login.vue
@@ -1,6 +1,13 @@
 <template>
-  <div style="height:100%">
-    
+  <div style="height:100%; margin:6% 15% 10% 15%">
+    <div class="body-2" style="margin-left:10px; margin-bottom:5px;">Login Information</div>
+    <v-card>
+      <v-card-text style="padding-left:20px;">
+        <v-text-field label="Username" required></v-text-field>
+        <v-text-field label="Password" required style="padding-top:0px;"></v-text-field>
+        <v-text-field label="Hostname" required style="padding-top:0px;"></v-text-field>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
--- client/src/components/client/components/header/rights/Resources.vue
+++ client/src/components/client/components/header/rights/Resources.vue
@@ -1,6 +1,15 @@
 <template>
-  <div style="height:100%">
-    
+  <div style="height:100%; margin:6% 15% 10% 15%">
+    <div class="body-2" style="margin-left:10px; margin-bottom:5px;">Resource Limits</div>
+    <v-card>
+      <v-card-text style="padding-left:20px;">
+        <v-text-field label="Max Queries / hour" required></v-text-field>
+        <v-text-field label="Max Updates / hour" required style="padding-top:0px;"></v-text-field>
+        <v-text-field label="Max Connections / hour" required style="padding-top:0px;"></v-text-field>
+        <v-text-field label="Max Simultaneous Connections / hour" required style="padding-top:0px;"></v-text-field>
+        <div class="body-2">Remarks: 0 (default) = no limit</div>
+      </v-card-text>
+    </v-card>
   </div>
 </template>
 
