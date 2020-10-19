40ea962bcbed059d7413c6bba7e1da990cff5886
Pol Alzina <gogs@fake.local>
2020-10-19 08:38:23 +0000

    More code

--- client/src/components/client/components/header/Rights.vue
+++ client/src/components/client/components/header/Rights.vue
@@ -5,7 +5,7 @@
         <v-toolbar flat color="primary">
           <v-toolbar-title class="white--text"><v-icon small style="padding-right:10px; padding-bottom:4px">fas fa-shield-alt</v-icon>User Rights</v-toolbar-title>
           <v-divider class="mx-3" inset vertical></v-divider>
-          <v-btn :disabled="!saveEnabled" color="primary" style="margin-right:10px;">Save</v-btn>
+          <v-btn :disabled="!saveEnabled" @click="saveClick" color="primary" style="margin-right:10px;">Save</v-btn>
           <v-spacer></v-spacer>
           <v-btn @click="dialog = false" icon><v-icon>fas fa-times-circle</v-icon></v-btn>
         </v-toolbar>
@@ -242,6 +242,9 @@ export default {
         // Reload Rights
         EventBus.$emit('reload-rights')
       }
+    },
+    saveClick() {
+      console.log(this.rightsItem)
     },
   }
 }
--- client/src/components/client/components/header/rights/Login.vue
+++ client/src/components/client/components/header/rights/Login.vue
@@ -3,16 +3,16 @@
     <div class="body-2" style="margin-left:10px; margin-bottom:5px;">Login Information</div>
     <v-card>
       <v-card-text style="padding-left:20px;">
-        <v-text-field v-model="login['username']" label="Username" required></v-text-field>
+        <v-text-field :disabled="disabled" v-model="login['username']" label="Username" required></v-text-field>
         <v-row no-gutters>
           <v-col class="flex-grow-1 flex-shrink-1">
-            <v-text-field v-model="login['password']" label="Password" :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" :type="showPassword ? 'text' : 'password'" @click:append="showPassword = !showPassword" required style="padding-top:0px;"></v-text-field>
+            <v-text-field :disabled="disabled" v-model="login['password']" label="Password" :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'" :type="showPassword ? 'text' : 'password'" @click:append="showPassword = !showPassword" required style="padding-top:0px;"></v-text-field>
           </v-col>
           <v-col cols="3" class="flex-grow-0 flex-shrink-1" style="margin-left:10px">
-            <v-select v-model="login['passwordType']" label="Type" :items="['String','Hash']" hide-details style="padding:0px; margin-bottom:5px;"></v-select>
+            <v-select :disabled="disabled" v-model="login['passwordType']" label="Type" :items="['String','Hash']" hide-details style="padding:0px; margin-bottom:5px;"></v-select>
           </v-col>
         </v-row>
-        <v-text-field v-model="login['hostname']" label="Hostname" required style="padding-top:0px;"></v-text-field>
+        <v-text-field :disabled="disabled" v-model="login['hostname']" label="Hostname" required style="padding-top:0px;"></v-text-field>
       </v-card-text>
     </v-card>
   </div>
@@ -25,6 +25,7 @@ import { mapFields } from '../../../js/map-fields'
 export default {
   data() {
     return {
+      disabled: true,
       login: {},
       showPassword: false,
     }
@@ -33,12 +34,16 @@ export default {
     ...mapFields([
       'rights',
       'rightsItem',
+      'rightsSelected',
     ], { path: 'client/connection' }),
   },
   mounted() {
     EventBus.$on('reload-rights', this.reloadRights);
   },
   watch: {
+    rightsSelected: function(val) {
+      this.disabled = Object.keys(val).length == 0 ? true : false
+    },
     login: {
       handler(obj) {
         // Compute diff
