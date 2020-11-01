c7d21aefaae84a4d501bd831d4f677a908d72b64
Pol Alzina <gogs@fake.local>
2020-11-01 18:44:11 +0000

    More code

--- client/src/components/monitoring/Navigation.vue
+++ client/src/components/monitoring/Navigation.vue
@@ -1,13 +1,16 @@
 <template>
   <v-main>
     <div>
-      <v-tabs background-color="#fa8131" color="white" v-model="tabs" slider-color="white" slot="extension" class="elevation-2"> <!-- #16a085 -->
+      <v-tabs background-color="#ed7b2f" color="white" v-model="tabs" slider-color="white" slot="extension" class="elevation-2"> <!-- #16a085 -->
         <v-tabs-slider></v-tabs-slider>
           <v-tab to="/monitoring"><span class="pl-2 pr-2"><v-icon small style="padding-right:10px">fas fa-desktop</v-icon>MONITORING</span></v-tab>
           <v-divider class="mx-3" inset vertical></v-divider>
           <v-tab to="/monitoring/parameters"><span class="pl-2 pr-2">Parameters</span></v-tab>
+          <v-divider class="mx-3" inset vertical></v-divider>
           <v-tab to="/monitoring/processlist"><span class="pl-2 pr-2">Processlist</span></v-tab>
+          <v-divider class="mx-3" inset vertical></v-divider>
           <v-tab to="/monitoring/queries"><span class="pl-2 pr-2">Queries</span></v-tab>
+          <v-divider class="mx-3" inset vertical></v-divider>
         </v-tabs>
     </div>
     <v-container fluid>
