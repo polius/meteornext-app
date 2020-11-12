0ee2e6bd4d057ff9f0ab928bb4577830c9c8386b
Pol Alzina <gogs@fake.local>
2020-11-12 12:00:58 +0000

    More code

--- client/src/components/client/components/bottombar/connections/New.vue
+++ client/src/components/client/components/bottombar/connections/New.vue
@@ -7,6 +7,8 @@
       <v-card>
         <v-toolbar flat color="primary">
           <v-toolbar-title class="white--text">New Connection</v-toolbar-title>
+          <v-divider class="mx-3" inset vertical></v-divider>
+          <v-btn :disabled="loading" :loading="disabled" @click="newConnectionSubmit" color="primary" style="margin-right:10px;">Save</v-btn>
           <v-spacer></v-spacer>
           <v-btn icon @click="dialog = false"><v-icon>fas fa-times-circle</v-icon></v-btn>
         </v-toolbar>
@@ -14,29 +16,35 @@
           <v-container style="padding:0px; max-width:100%;">
             <v-layout wrap>
               <v-flex xs12>
-                <v-form ref="form" style="margin-top:5px; margin-bottom:15px;">
-                  <div class="body-1" style="margin-left:3px;">Select servers from the <router-link class="nav-link" to="/inventory/servers" target="_blank">inventory</router-link> to add.</div>
-                  <v-card style="margin-top:15px">
+                <v-form ref="form" style="margin-top:5px; margin-bottom:10px;">
+                  <v-card>
                     <v-toolbar flat dense color="#2e3131">
                       <v-toolbar-title class="white--text">SERVERS</v-toolbar-title>
                       <v-divider class="mx-3" inset vertical></v-divider>
-                      <v-text-field v-model="search" append-icon="search" label="Search" color="white" single-line hide-details></v-text-field>
+                      <v-text-field v-model="search" @input="onServerSearch" append-icon="search" label="Search" color="white" single-line hide-details></v-text-field>
                     </v-toolbar>
                     <v-card-text style="padding:0px;">
-                      <Splitpanes @ready="onSplitPaneReady" style="height:58vh">
-                        <Pane size="40" min-size="0" style="align-items:inherit">
+                      <Splitpanes @ready="onSplitPaneReady" style="height:71vh">
+                        <Pane size="30" min-size="0" style="align-items:inherit">
                           <v-container fluid style="padding:0px;">
-                            <v-row ref="list" no-gutters style="height:calc(100% - 36px); overflow:auto;">
-                              <!-- <v-list style="width:100%; padding:0px;">
-                                <v-list-item-group v-model="selected" mandatory multiple>
-                                  <v-list-item v-for="(item, i) in items" :key="i" dense :ref="'saved' + i" @click="onListClick($event, i)" @contextmenu="onListRightClick">
-                                    <v-list-item-content><v-list-item-title v-text="item.name"></v-list-item-title></v-list-item-content>
+                            <v-row no-gutters style="height:calc(100% - 36px); overflow:auto;">
+                              <v-list flat style="width:100%; padding:0px;">
+                                <v-list-item-group mandatory multiple>
+                                  <v-list-item v-for="item in items" :key="item.id" dense @click="onServerClick(item)" @contextmenu="$event.preventDefault()" style="max-height:20px;">
+                                    <template>
+                                      <v-list-item-action style="margin-right:15px">
+                                        <v-checkbox dense :input-value="selected.includes(item.id)"></v-checkbox>
+                                      </v-list-item-action>
+                                      <v-list-item-content>
+                                        <v-list-item-title><v-icon small :title="item.shared ? 'Shared' : 'Personal'" :color="item.shared ? 'error' : 'warning'" style="margin-right:10px; margin-bottom:2px">{{ item.shared ? 'fas fa-users' : 'fas fa-user' }}</v-icon>{{ item.name }}</v-list-item-title>
+                                      </v-list-item-content>
+                                    </template>
                                   </v-list-item>
                                 </v-list-item-group>
-                              </v-list> -->
+                              </v-list>
                             </v-row>
                             <v-row no-gutters style="height:35px; border-top:2px solid #3b3b3b; width:100%">
-                              <v-btn @click="refresh" text small title="Refresh Servers" style="height:30px; min-width:36px; margin-top:1px; margin-left:2px; margin-right:2px;"><v-icon small style="font-size:12px;">fas fa-redo-alt</v-icon></v-btn>
+                              <v-btn @click="refresh" :loading="loading" :disabled="loading" text small title="Refresh Servers" style="height:30px; min-width:36px; margin-top:1px; margin-left:2px; margin-right:2px;"><v-icon small style="font-size:12px;">fas fa-redo-alt</v-icon></v-btn>
                               <span style="background-color:#3b3b3b; padding-left:1px;margin-left:1px; margin-right:1px;"></span>
                               <v-btn @click="selectAll" text small title="Select All" style="height:30px; min-width:36px; margin-top:1px; margin-left:2px; margin-right:2px;"><v-icon small style="font-size:12px;">fas fa-check-square</v-icon></v-btn>
                               <span style="background-color:#3b3b3b; padding-left:1px;margin-left:1px; margin-right:1px;"></span>
@@ -45,13 +53,14 @@
                             </v-row>
                           </v-container>
                         </Pane>
-                        <Pane size="80" min-size="0" style="background-color:#484848">
+                        <Pane size="70" min-size="0" style="background-color:#484848">
+
                         </Pane>
                       </Splitpanes>
                     </v-card-text>
                   </v-card>
                 </v-form>
-                <v-divider></v-divider>
+                <!-- <v-divider></v-divider>
                 <div style="margin-top:15px;">
                   <v-row no-gutters>
                     <v-col cols="auto" style="margin-right:5px; margin-bottom:10px;">
@@ -61,7 +70,7 @@
                       <v-btn :disabled="loading" @click="dialog = false" outlined color="#e74d3c">Cancel</v-btn>
                     </v-col>
                   </v-row>
-                </div>
+                </div> -->
               </v-flex>
             </v-layout>
           </v-container>
@@ -89,7 +98,10 @@ export default {
       // Dialog
       dialog: false,
       search: '',
+      servers: [],
       items: [],
+      selected: [],
+      display: {},
     }
   },
   components: { Splitpanes, Pane },
@@ -97,6 +109,9 @@ export default {
     ...mapFields([
       'sidebarSelected',
     ], { path: 'client/connection' }),
+  },
+  created() {
+    this.getServers()
   },
   mounted() {
     EventBus.$on('show-bottombar-connections-new', this.newConnection)
@@ -114,15 +129,26 @@ export default {
 
     },
     getServers() {
+      this.loading = true
       axios.get('/inventory/servers')
         .then((response) => {
+          this.servers = response.data.data
           this.items = response.data.data
-          this.loading = false
         })
         .catch((error) => {
           if (error.response === undefined || error.response.status != 400) this.$store.dispatch('app/logout').then(() => this.$router.push('/login'))
           else this.notification(error.response.data.message, 'error')
         })
+        .finally(() => this.loading = false )
+    },
+    onServerSearch(value) {
+      if (value.length == 0) this.items = this.servers.slice(0)
+      else this.items = this.servers.filter(x => x.name.toLowerCase().includes(value.toLowerCase()))
+    },
+    onServerClick(item) {
+      const index = this.selected.findIndex(x => x == item.id)
+      if (index > -1) this.selected.splice(index, 1)
+      else this.selected.push(item.id)
     },
     newConnection() {
       this.dialog = true
@@ -131,13 +157,13 @@ export default {
       this.dialog = false
     },
     refresh() {
-
+      this.getServers()
     },
     selectAll() {
-
+      this.selected = this.servers.map(x => x.id)
     },
     deselectAll() {
-
+      this.selected = []
     },
   }
 }
