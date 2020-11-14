4bf7edb6df8c4577ba0917985e164c96f934fd5b
Pol Alzina <gogs@fake.local>
2020-11-14 17:28:29 +0000

    More code

--- client/src/components/inventory/views/Environments.vue
+++ client/src/components/inventory/views/Environments.vue
@@ -19,7 +19,7 @@
       <v-data-table v-model="selected" :headers="headers" :items="items" :search="search" :loading="loading" loading-text="Loading... Please wait" item-key="name" show-select class="elevation-1" style="padding-top:3px;">
         <template v-slot:[`item.servers`]="{ item }">
           <div v-for="server in item.servers" :key="server.id" style="margin-left:0px; padding-left:0px; float:left; margin-right:5px; padding-top:3px; padding-bottom:3px;">
-            <v-chip outlined :color="server.color" style="margin-left:0px;"><span class="font-weight-medium" style="padding-right:4px;">{{ server.server }}</span> - {{ server.region }}</v-chip>
+            <v-chip outlined label :color="server.color" style="margin-left:0px;"><span class="font-weight-medium" style="padding-right:4px;">{{ server.server }}</span> - {{ server.region }}</v-chip>
           </div>
         </template>
         <template v-slot:[`item.shared`]="{ item }">
