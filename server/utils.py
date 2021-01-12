ba9cd2c9fedcdf2d856f6aa6acd4f876d4e33485
Pol Alzina <gogs@fake.local>
2021-01-12 19:52:21 +0000

    More code

--- server/utils.py
+++ server/utils.py
@@ -1,52 +1,8 @@
-from io import StringIO
 import os
-import sys
-import time
-import paramiko
-import pymysql
-import sshtunnel
 
 class Utils:
-    def __init__(self, connection=None):
-        self._conn = connection
-
-    def check_ssh(self):
-        # Supress Errors Output
-        sys_stderr = sys.stderr
-        sys.stderr = open(os.devnull, 'w')
-
-        try:
-            pkey = None if self._conn['key'] is None or len(self._conn['key'].strip()) == 0 else paramiko.RSAKey.from_private_key(StringIO(self._conn['key']), password=self._conn['password'])
-            client = paramiko.SSHClient()
-            client.load_system_host_keys()
-            client.set_missing_host_key_policy(paramiko.WarningPolicy())
-            client.connect(hostname=self._conn['hostname'], port=int(self._conn['port']), username=self._conn['username'], password=self._conn['password'], pkey=pkey, timeout=10)
-            client.close()
-        finally:
-            # Supress Errors Output
-            sys_stderr = sys.stderr
-            sys.stderr = open(os.devnull, 'w')
-
-    def check_sql(self, server):
-        f = open(os.devnull, 'w')
-        stdout = sys.stdout
-        stderr = sys.stderr
-        sys.stdout = f
-        sys.stderr = f
-
-        try:
-            if self._conn:
-                pkey = None if self._conn['key'] is None or len(self._conn['key'].strip()) == 0 else paramiko.RSAKey.from_private_key(StringIO(self._conn['key']), password=self._conn['password'])
-                sshtunnel.SSH_TIMEOUT = 10.0
-                with sshtunnel.SSHTunnelForwarder((self._conn['hostname'], int(self._conn['port'])), ssh_username=self._conn['username'], ssh_password=self._conn['password'], ssh_pkey=pkey, remote_bind_address=(server['hostname'], int(server['port'])), threaded=False) as tunnel:
-                    conn = pymysql.connect(host='127.0.0.1', port=tunnel.local_bind_port, user=server['username'], passwd=server['password'])
-                    conn.close()
-            else:
-                conn = pymysql.connect(host=server['hostname'], port=int(server['port']), user=server['username'], passwd=server['password'])
-            return {"success": True}
-        finally:
-            sys.stdout = stdout
-            sys.stderr = stderr
+    def __init__(self):
+        pass
 
     def check_local_path(self, path):
         while not os.path.exists(path) and path != '/':
