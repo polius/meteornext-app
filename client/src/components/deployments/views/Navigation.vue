4ebb6bbb3411000802aafe461fef1613adacc324
Pol Alzina <gogs@fake.local>
2021-03-17 12:13:30 +0000

    More code

--- client/src/components/deployments/views/Navigation.vue
+++ client/src/components/deployments/views/Navigation.vue
@@ -5,8 +5,8 @@
         <v-toolbar-title class="subtitle-1">NEW DEPLOY</v-toolbar-title>
         <v-divider class="mx-3" inset vertical></v-divider>
         <div>
-          <v-btn v-if="deployments_basic" :color="basicColor" @click="basic()" style="margin-right:10px;">Basic</v-btn>
-          <v-btn v-if="deployments_pro" :color="proColor" @click="pro()" style="margin-right:10px;">Pro</v-btn>
+          <v-btn v-if="deployments_basic" :color="basicColor" @click="basic()"><v-icon small style="padding-right:10px; padding-bottom:1px">fas fa-chess-knight</v-icon>Basic</v-btn>
+          <v-btn v-if="deployments_pro" :color="proColor" @click="pro()" style="margin-left:10px;"><v-icon small style="padding-right:10px; padding-bottom:1px">fas fa-chess-queen</v-icon>Pro</v-btn>
         </div>
         <v-spacer></v-spacer>
         <router-link class="nav-link" to="/deployments"><v-btn icon><v-icon size="22">fas fa-times-circle</v-icon></v-btn></router-link>
