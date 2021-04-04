66e436a1197af151c068e15c7bad96cdf8623b8f
Pol Alzina <gogs@fake.local>
2021-04-04 10:50:25 +0000

    Tokens var clean

--- client/src/store/modules/app.js
+++ client/src/store/modules/app.js
@@ -6,7 +6,6 @@ const state = () => ({
   isLoggedIn: Cookies.get('csrf_access_token') !== undefined,
   username: localStorage.getItem('username') || '',
   remember: localStorage.getItem('remember') == '1' ? true : false,
-  // token: localStorage.getItem('token') || '',
   coins: localStorage.getItem('coins') || 0,
   admin: localStorage.getItem('admin') == '1' ? true : false,
   owner: localStorage.getItem('owner') == '1' ? true : false,
@@ -15,7 +14,6 @@ const state = () => ({
   deployments_enabled: localStorage.getItem('deployments_enabled') == '1' ? true : false,
   deployments_basic: localStorage.getItem('deployments_basic') == '1' ? true : false,
   deployments_pro: localStorage.getItem('deployments_pro') == '1' ? true : false,
-  deployments_inbenta: localStorage.getItem('deployments_inbenta') == '1' ? true : false,
   monitoring_enabled: localStorage.getItem('monitoring_enabled') == '1' ? true : false,
   utils_enabled: localStorage.getItem('utils_enabled') == '1' ? true : false,
   client_enabled: localStorage.getItem('client_enabled') == '1' ? true : false,
@@ -36,7 +34,6 @@ const getters = {
   deployments_enabled: state => state.deployments_enabled,
   deployments_basic: state => state.deployments_basic,
   deployments_pro: state => state.deployments_pro,
-  deployments_inbenta: state => state.deployments_inbenta,
   monitoring_enabled: state => state.monitoring_enabled,
   utils_enabled: state => state.utils_enabled,
   client_enabled: state => state.client_enabled,
@@ -57,7 +54,6 @@ const actions = {
           var data = {
             username: response.data.data.username,
             remember: user['remember'],
-            // token: response.data.data.access_token,
             coins: response.data.data.coins,
             admin: response.data.data.admin,
             owner: response.data.data.owner,
@@ -66,7 +62,6 @@ const actions = {
             deployments_enabled: response.data.data.deployments_enabled,
             deployments_basic: response.data.data.deployments_basic,
             deployments_pro: response.data.data.deployments_pro,
-            deployments_inbenta: response.data.data.deployments_inbenta,
             monitoring_enabled: response.data.data.monitoring_enabled,
             utils_enabled: response.data.data.utils_enabled,
             client_enabled: response.data.data.client_enabled,
@@ -76,7 +71,6 @@ const actions = {
           // Store variables to the local storage
           localStorage.setItem('username', data['username'])
           localStorage.setItem('remember', user['remember'] ? '1' : '0')
-          // localStorage.setItem('token', data['token'])
           localStorage.setItem('coins', data['coins'])
           localStorage.setItem('admin', data['admin'])
           localStorage.setItem('owner', data['owner'])
@@ -85,7 +79,6 @@ const actions = {
           localStorage.setItem('deployments_enabled', data['deployments_enabled'])
           localStorage.setItem('deployments_basic', data['deployments_basic'])
           localStorage.setItem('deployments_pro', data['deployments_pro'])
-          localStorage.setItem('deployments_inbenta', data['deployments_inbenta'])
           localStorage.setItem('monitoring_enabled', data['monitoring_enabled'])
           localStorage.setItem('utils_enabled', data['utils_enabled'])
           localStorage.setItem('client_enabled', data['client_enabled'])
@@ -93,7 +86,6 @@ const actions = {
           localStorage.setItem('coins_day', data['coins_day'])
 
           // Add the token to the axios lib
-          // axios.defaults.headers.common['Authorization'] = `Bearer ${data['token']}`
           axios.defaults.headers.common['X-CSRF-TOKEN'] = Cookies.get('csrf_access_token')
           // Store variables to vuex
           commit('auth', data)
@@ -103,7 +95,6 @@ const actions = {
           commit('logout')
           // Remove variables from the local storage
           if (!user['remember']) localStorage.removeItem('username')
-          // localStorage.removeItem('token')
           localStorage.removeItem('coins')
           localStorage.removeItem('admin')
           localStorage.removeItem('owner')
@@ -112,7 +103,6 @@ const actions = {
           localStorage.removeItem('deployments_enabled')
           localStorage.removeItem('deployments_basic')
           localStorage.removeItem('deployments_pro')
-          localStorage.removeItem('deployments_inbenta')
           localStorage.removeItem('monitoring_enabled')
           localStorage.removeItem('utils_enabled')
           localStorage.removeItem('client_enabled')
@@ -136,7 +126,6 @@ const actions = {
         commit('logout')
         // Remove variables from the local storage
         if (!getters.remember) localStorage.removeItem('username')
-        // localStorage.removeItem('token')
         localStorage.removeItem('coins')
         localStorage.removeItem('admin')
         localStorage.removeItem('owner')
@@ -145,7 +134,6 @@ const actions = {
         localStorage.removeItem('deployments_enabled')
         localStorage.removeItem('deployments_basic')
         localStorage.removeItem('deployments_pro')
-        localStorage.removeItem('deployments_inbenta')
         localStorage.removeItem('monitoring_enabled')
         localStorage.removeItem('utils_enabled')
         localStorage.removeItem('client_enabled')
@@ -166,7 +154,6 @@ const mutations = {
     state.isLoggedIn = true
     state.username = data.username
     state.remember = data.remember == 1
-    // state.token = data.token
     state.coins = data.coins
     state.admin = data.admin == 1
     state.owner = data.owner == 1
@@ -175,7 +162,6 @@ const mutations = {
     state.deployments_enabled = data.deployments_enabled == 1
     state.deployments_basic = data.deployments_basic == 1
     state.deployments_pro = data.deployments_pro == 1
-    state.deployments_inbenta = data.deployments_inbenta == 1
     state.monitoring_enabled = data.monitoring_enabled == 1
     state.utils_enabled = data.utils_enabled == 1
     state.client_enabled = data.client_enabled == 1
@@ -185,7 +171,6 @@ const mutations = {
   logout(state) {
     state.isLoggedIn = false
     state.username = (state.remember) ? state.username : ''
-    // state.token = ''
     state.coins = 0
     state.admin = 0
     state.owner = 0
@@ -194,7 +179,6 @@ const mutations = {
     state.deployments_enabled = 0
     state.deployments_basic = 0
     state.deployments_pro = 0
-    state.deployments_inbenta = 0
     state.monitoring_enabled = 0
     state.utils_enabled = 0
     state.client_enabled = 0
