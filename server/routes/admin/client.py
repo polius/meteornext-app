8a5ea7b3e9fd18a89ccd8149d41cd172935da82f
Pol Alzina <gogs@fake.local>
2021-05-10 17:38:17 +0000

    Start implementing Admin/Client

---
+++ server/routes/admin/client.py
@@ -0,0 +1,48 @@
+import json
+from flask import Blueprint, jsonify, request
+from flask_jwt_extended import (jwt_required, get_jwt_identity)
+
+import models.admin.users
+import models.admin.client
+import routes.admin.settings
+
+class Client:
+    def __init__(self, app, sql, license):
+        self._app = app
+        self._sql = sql
+        self._license = license
+        # Init models
+        self._users = models.admin.users.Users(sql)
+        self._client = models.admin.client.Client(sql)
+        # Init routes
+        self._settings = routes.admin.settings.Settings(app, sql, license)
+
+    def blueprint(self):
+        # Init blueprint
+        admin_client_blueprint = Blueprint('admin_client', __name__, template_folder='admin_client')
+
+        @admin_client_blueprint.route('/admin/client', methods=['GET'])
+        @jwt_required()
+        def admin_client_method():
+            # Check license
+            if not self._license.validated:
+                return jsonify({"message": self._license.status['response']}), 401
+
+            # Check Settings - Security (Administration URL)
+            if not self._settings.check_url():
+                return jsonify({'message': 'Insufficient Privileges'}), 401
+
+            # Get user data
+            user = self._users.get(get_jwt_identity())[0]
+
+            # Check user privileges
+            if user['disabled'] or not user['admin']:
+                return jsonify({'message': 'Insufficient Privileges'}), 401
+
+            # Get Request Json
+            settings_json = request.get_json()
+
+            # Return Client Queries
+            return jsonify({'queries': self._client.get(request.args), 'users': self._client.getUsers()}), 200
+
+        return admin_client_blueprint
