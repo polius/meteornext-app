af94f0bfa904a93dd2bcbdf2c0705ced51c1db16
Pol Alzina <pol.alzina@gmail.com>
2021-05-28 13:29:49 +0000

    Client BottomBar now shows correctly the rows affected

--- client/src/components/client/components/main/Client.vue
+++ client/src/components/client/components/main/Client.vue
@@ -1077,7 +1077,7 @@ export default {
       }
       current.bottomBar.client['status'] = this.clientQueryStopped ? 'stopped' : data[data.length-1]['error'] === undefined ? 'success' : 'failure'
       current.bottomBar.client['text'] = data[data.length-1]['query'].trim().endsWith(';') ? data[data.length-1]['query'].trim() : data[data.length-1]['query'].trim() + ';'
-      current.bottomBar.client['info'] = current.clientItems.length + ' records | '
+      current.bottomBar.client['info'] = data[data.length-1]['rowCount'] + ' records | '
       current.bottomBar.client['info'] += data.length + ' queries'
       if (elapsed != null) current.bottomBar.client['info'] += ' | ' + elapsed.toFixed(3).toString() + 's elapsed'
     },
