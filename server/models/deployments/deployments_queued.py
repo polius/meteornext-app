9360e96365b7404a5d4df8098720c2ccab245cb1
Pol Alzina <pol.alzina@gmail.com>
2021-06-18 17:12:28 +0000

    Deployments: Improved Queue System

--- server/models/deployments/deployments_queued.py
+++ server/models/deployments/deployments_queued.py
@@ -32,18 +32,16 @@ class Deployments_Queued:
 
     def getNext(self):
         query = """
-            SELECT SUBSTRING_INDEX(GROUP_CONCAT(CONCAT(q.execution_mode, '|', q.execution_id, '|', q.status) ORDER BY q.id SEPARATOR ','), ',', COALESCE(g.deployments_execution_concurrent,1)) AS 'executions'
-            FROM
-            (
+            SELECT q.execution_mode AS 'mode', q.execution_id AS 'id', q.status, g.id AS 'group', g.deployments_execution_concurrent AS 'concurrent'
+            FROM (
                 SELECT q.id, q.execution_mode, q.execution_id, b.status, b.deployment_id FROM deployments_queued q JOIN deployments_basic b ON b.id = q.execution_id AND q.execution_mode = 'basic' AND b.status IN('QUEUED','STARTING','IN PROGRESS','STOPPING')
-                UNION
+                UNION ALL
                 SELECT q.id, q.execution_mode, q.execution_id, p.status, p.deployment_id FROM deployments_queued q JOIN deployments_pro p ON p.id = q.execution_id AND q.execution_mode = 'pro' AND p.status IN('QUEUED','STARTING','IN PROGRESS','STOPPING')
-                ORDER BY id
             ) q
             JOIN deployments d ON d.id = q.deployment_id
             JOIN users u ON u.id = d.user_id
             JOIN groups g ON g.id = u.group_id
-            GROUP BY g.id
+            ORDER BY q.id
         """
         return self._sql.execute(query)
 
