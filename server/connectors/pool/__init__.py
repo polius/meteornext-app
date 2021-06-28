007539ad6a4afd57eaf1f86e1722787214e533e2
Pol Alzina <gogs@fake.local>
2021-06-28 10:56:08 +0000

    PooledDB Core Improvements

--- server/connectors/pool/__init__.py
+++ server/connectors/pool/__init__.py
@@ -5,17 +5,8 @@ class Pool:
         if config['engine'] in ['MySQL','Aurora MySQL']:
             self._sql = MySQL(config)
 
-    def execute(self, query, args=None, database=None, conn=None):
+    def execute(self, query, args=None, database=None):
         return self._sql.execute(query, args, database)
 
     def mogrify(self, query, args=None):
         return self._sql.mogrify(query, args)
-
-    def transaction(self):
-        return self._sql.transaction()
-
-    def commit(self, conn):
-        return self._sql.commit(conn)
-
-    def rollback(self, conn):
-        return self._sql.rollback(conn)
