7028650afd70fba8306e56a2726e39d8ecb726f7
Pol Alzina <gogs@fake.local>
2021-12-10 15:01:18 +0000

    Minor corrections

--- server/routes/deployments/blueprint.py
+++ server/routes/deployments/blueprint.py
@@ -11,25 +11,25 @@ class blueprint:
         }
 
     #######################################################
-    # Executed once per Server before all main executions #
+    # Executed once per server before all main executions #
     #######################################################
     def before(self, meteor, environment, region, server):
         pass
 
     ##############################
-    # Executed once per Database #
+    # Executed once per database #
     ##############################
     def main(self, meteor, environment, region, server, database):
         pass
 
     ######################################################
-    # Executed once per Server after all main executions #
+    # Executed once per server after all main executions #
     ######################################################
     def after(self, meteor, environment, region, server):
         pass
 
     ########################
-    # User Defined Methods #
+    # User defined methods #
     ########################
     def search(self, items, key, value):
         # Search a key value in a list of dictionaries
