dc7a8e63dadf8fa23aa463aeb3fb03f185bb714f
Pol Alzina <pol.alzina@gmail.com>
2022-02-03 11:44:02 +0000

    Client UI Improvements and Bug fixes

--- client/src/components/client/components/bottombar/objects/Variables.vue
+++ client/src/components/client/components/bottombar/objects/Variables.vue
@@ -25,21 +25,25 @@
     <!-------------------->
     <v-dialog v-model="confirmDialog" max-width="50%">
       <v-card>
-        <v-card-text style="padding:15px 15px 5px;">
+        <v-toolbar dense flat color="primary">
+          <v-toolbar-title class="white--text subtitle-1"><v-icon small style="margin-right:10px; margin-bottom:3px">fas fa-exclamation-triangle</v-icon>CONFIRMATION</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-btn :disabled="loading" @click="confirmDialogCancel" icon><v-icon size="22">fas fa-times-circle</v-icon></v-btn>
+        </v-toolbar>
+        <v-card-text style="padding:15px">
           <v-container style="padding:0px; max-width:100%;">
             <v-layout wrap>
-              <div class="text-h6" style="font-weight:400;">Confirmation</div>
               <v-flex xs12>
-                <div class="body-1" style="font-weight:300; font-size:1.05rem!important; margin-top:10px;">Are you sure you want to change this server variable?</div>
+                <div class="body-1">Are you sure you want to change this server variable?</div>
                 <v-text-field v-model="currentCellValues.current" filled readonly label="Current Value" hide-details style="margin-top:15px;"></v-text-field>
                 <v-text-field v-model="currentCellValues.new" filled readonly label="New Value" hide-details style="margin-top:15px; margin-bottom:15px;"></v-text-field>
                 <v-divider></v-divider>
                 <div style="margin-top:15px;">
                   <v-row no-gutters>
-                    <v-col cols="auto" style="margin-right:5px; margin-bottom:10px;">
+                    <v-col cols="auto" style="margin-right:5px">
                       <v-btn :loading="loading" @click="confirmDialogSubmit" color="#00b16a">Confirm</v-btn>
                     </v-col>
-                    <v-col style="margin-bottom:10px;">
+                    <v-col>
                       <v-btn :disabled="loading" @click="confirmDialogCancel" color="#EF5354">Cancel</v-btn>
                     </v-col>
                   </v-row>
