e4f5d193252e88be2acf76b1049099a848adc9fe
Pol Alzina <pol.alzina@gmail.com>
2022-02-13 20:05:19 +0000

    Fixed a Client bug fix

--- server/routes/client/client.py
+++ server/routes/client/client.py
@@ -50,7 +50,7 @@ class Client:
                 servers_secured = []
                 for s in servers:
                     if s['secured']:
-                        servers_secured.append({"id": s['id'], "name": s['name'], "region_id": s['region_id'], "region": s['region'], "shared": s['shared'], "secured": s['secured'], "active": s['active'], "region_shared": s['region_shared'], "region_secured": s['region_secured'], "ssl": s['ssl'], "ssh": s['ssh'], "folder_id": s['folder_id'], "folder_name": s['folder_name']})
+                        servers_secured.append({"id": s['id'], "name": s['name'], "engine": s['engine'], "region_id": s['region_id'], "region": s['region'], "shared": s['shared'], "secured": s['secured'], "active": s['active'], "region_shared": s['region_shared'], "region_secured": s['region_secured'], "ssl": s['ssl'], "ssh": s['ssh'], "folder_id": s['folder_id'], "folder_name": s['folder_name']})
                     else:
                         servers_secured.append(s)
                 return jsonify({'servers': servers_secured, 'folders': folders}), 200
