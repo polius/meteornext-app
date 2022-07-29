2a0d592b4283484d4b147748806d49ce568077eb
Pol Alzina <gogs@fake.local>
2022-07-29 08:37:10 +0000

    Improved Admin-Deployments&Utils UX

--- client/src/components/admin/views/utils/Clones.vue
+++ client/src/components/admin/views/utils/Clones.vue
@@ -476,7 +476,13 @@ export default {
       return pretty(size, {binary: true}).replace('i','')
     },
     infoClone() {
-      this.$router.push({ name: 'utils.clones.info', params: { uri: this.selected[0]['uri'] }})
+      for (let i = 0; i < this.selected.length; ++i) {
+        const route = this.$router.resolve({
+          name: "utils.clones.info",
+          params: { uri: this.selected[i]['uri'] },
+        });
+        window.open(route.href, "_blank")
+      }
     },
     getServer(server_id) {
       EventBus.$emit('utils-get-server', server_id)
