06524d58898785a55422250e79a81e9db64f05a5
Pol Alzina <gogs@fake.local>
2022-10-27 16:42:42 +0000

    Improved Client Extended Editor

--- server/server.py
+++ server/server.py
@@ -6,7 +6,7 @@ import argparse
 class server:
     def __init__(self):
         # Init Global Variables
-        self._version = '1.00-beta.52'
+        self._version = '1.00-beta.53'
         self._sentry_dsn = 'https://7de474b9a31148d29d10eb5aea1dff71@o1100742.ingest.sentry.io/6138582'
 
         # Init args
